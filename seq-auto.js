99aad96e88becb2487dc83034b690625fb3b6eab
karpawar <karpawar@gmail.com>
2020-01-30 13:59:28 +0530

    added sequlize js generator

---
+++ seq-auto.js
@@ -0,0 +1,30 @@
+var SequelizeAuto = require('sequelize-auto')
+
+
+// var auto = new SequelizeAuto('classicmodels', 'root', 'root',{
+//   host: 'localhost',
+//   port:'3306'
+// });
+
+var auto = new SequelizeAuto('Inventory', 'postgres', 'root@123', {
+    host: 'localhost',
+    dialect:  'postgres', //'mysql'|'mariadb'|'sqlite'|'postgres'|'mssql', but install perticular dialect
+    directory: './models', // prevents the program from writing to disk
+    port: '5432',
+    additional: {
+        timestamps: false
+        //...
+    },
+    
+    //tables:['table1', 'table2', 'table3']
+    //...
+})
+
+
+auto.run(function (err) {
+  if (err) throw err;
+
+  console.log(auto.tables); // table list
+  console.log(auto.foreignKeys); // foreign key list
+});
+
